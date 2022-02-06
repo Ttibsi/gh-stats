14d2ce3f918664b889286ea295c63470020a2b35
Ttibsi <ashisbitt@icloud.com>
2022-02-06 18:45:32 +0000

    Check user's gh username

--- src/gh_stats/gh_stats.py
+++ src/gh_stats/gh_stats.py
@@ -5,8 +5,24 @@ def log(msg:str):
         file.write(msg + "\n")
 
 
+def get_username() -> str:
+    with open(os.path.expanduser("~/.gitconfig")) as git_file:
+        git_lines = git_file.readlines()
+
+    for line in git_lines:
+        if line.startswith("name"):
+            name_line = line.split(" = ")
+            break
+
+    return name_line[1].lower().strip()
+
+
 def main() -> int:
     log("start")
+
+    username = get_username()
+    log(f"{username=}")
+
     log("end")
     return 0
 
