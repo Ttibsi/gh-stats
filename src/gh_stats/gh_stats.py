ce760da807e6b4a9af59c2581029f9992473b14a
Ttibsi <ashisbitt@icloud.com>
2022-02-06 18:44:15 +0000

    simple and stupig logging

--- src/gh_stats/gh_stats.py
+++ src/gh_stats/gh_stats.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
+def log(msg:str):
+    with open('gh_stat.log', 'a') as file:
+        file.write(msg + "\n")
+
+
 def main() -> int:
+    log("start")
+    log("end")
     return 0
 
 if __name__ == '__main__':
