d01b14ba12c6fcae4f6cce13fbe720b36bd66f09
Ttibsi <ashisbitt@icloud.com>
2022-02-19 12:47:34 +0000

    First two unittests - still need to make sure they work

--- test/test_gh_stats.py
+++ test/test_gh_stats.py
@@ -1,6 +1,7 @@
 import pytest
 
 from src.gh_stats import gh_stats  # type: ignore
+from src.gh_stats import stats  # type: ignore
 
 
 def test_get_username():
@@ -11,3 +12,21 @@ def test_get_username():
 def test_get_current_year():
     """Is there a future proof way to test this?"""
     assert gh_stats.get_current_year() == 2022
+
+
+def test_count_commits():
+    res = 0
+    with open("test_data.json") as f:
+        for item in f:
+            res += gh_stats.count_commits(f)
+
+    assert res == 30
+
+
+def test_count_monthly():
+    res = 0
+    with open("test_data.json") as f:
+        for item in f:
+            res += gh_stats.count_monthly(f)
+
+    assert res == 30
