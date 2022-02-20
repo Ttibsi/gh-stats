a8ebcb4ea745ec9fe1812c2ccfd48395b642d3a8
Ttibsi <ashisbitt@icloud.com>
2022-02-20 14:31:20 +0000

    Two passing unittests

--- test/test_gh_stats.py
+++ test/test_gh_stats.py
@@ -1,24 +1,24 @@
 import pytest
 
-from src.gh_stats import gh_stats  # type: ignore
-from src.gh_stats import stats  # type: ignore
+from gh_stats import ghstat  # type: ignore
+from gh_stats import stats  # type: ignore
 
 
 def test_get_username():
     """This will only work on my system. This test could be better"""
-    assert gh_stats.get_username() == "ttibsi"
+    assert ghstat.get_username() == "ttibsi"
 
 
 def test_get_current_year():
     """Is there a future proof way to test this?"""
-    assert gh_stats.get_current_year() == 2022
+    assert ghstat.get_current_year() == 2022
 
 
 def test_count_commits():
     res = 0
     with open("test_data.json") as f:
         for item in f:
-            res += gh_stats.count_commits(f)
+            res += ghstat.count_commits(f)
 
     assert res == 30
 
@@ -27,6 +27,6 @@ def test_count_monthly():
     res = 0
     with open("test_data.json") as f:
         for item in f:
-            res += gh_stats.count_monthly(f)
+            res += ghstat.count_monthly(f)
 
     assert res == 30
