b3c597bb162475b1a65dba156e7e5808a46d12d8
ttibsi <ashisbitt@icloud.com>
2022-06-09 22:48:29 +0100

    Convert setup.py to cfg

--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
-from setuptools import find_packages
 from setuptools import setup
 
-
-setup(
-    name="gh-stats",
-    version="1.1.0",
-    description="Get statistics about your Github commit history",
-    author="Ttibsi",
-    author_email="ashisbitt@icloud.com",
-    url="https://github.com/ttibsi/gh-stats",
-    python_requires=">=3.7",
-    install_requires=[
-        "PyInputPlus",
-        "requests",
-    ],
-    packages=find_packages(
-        exclude=["test*"],
-    ),
-    entry_points={
-        "console_scripts": [
-            "ghstats = gh_stats.ghstats:main",
-        ],
-    },
-)
+setup()
